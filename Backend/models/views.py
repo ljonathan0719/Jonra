63eee68a2cec14e2d7f10553861de23d7b8c0a87
JonKissil <you@example.com>
2025-03-23 18:45:15 -0400

    file structure fixed

--- Backend/models/views.py
+++ Backend/models/views.py
@@ -2,8 +2,3 @@ from django.http import HttpResponse
 from django.template import loader
 
 from .models import Board
-
-
-def home(request):
-    template = loader.get_template("models/homepage.html")
-    return HttpResponse(template.render())
