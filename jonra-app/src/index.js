06cd8dd7a2ed9c011cbf154466fa8cef7d4706ac
JonKissil <you@example.com>
2025-03-27 11:35:52 -0400

    Routing between client-facing pages added

--- jonra-app/src/index.js
+++ jonra-app/src/index.js
@@ -2,26 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {BrowserRouter} from "react-router-dom"
 import App from "./App"
-import Board from "./pages/Board"
-import Layout from "./pages/Layout"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
-
-// export default function App() {
-//   return (
-//       <BrowserRouter>
-//           <Routes>
-//               <Route path="/" element ={<Layout />}>
-//                   <Route index element={<Home />} />
-//                   <Route path ="board" element={<Board />} />
-//                   <Route path ="login" element={<Login />} />
-//                   <Route path ="signup" element={<Signup />} />
-//               </Route>
-//           </Routes>
-//       </BrowserRouter>
-//   );
-// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
